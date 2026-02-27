2ffc475e189238737a6e93d51821bb05a1310ee1
ANosok <kirill60278@gmail.com>
2026-02-27 10:59:19 +0500

    2422

---
+++ Terminal/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Common;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Terminal
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<News> News = new List<News>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadNews();
+        }
+        public void GetContent()
+        {
+            IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        private void Get(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
